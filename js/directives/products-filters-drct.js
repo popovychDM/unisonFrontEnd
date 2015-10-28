ea0834ab41f9ed9f94125b94710e345d7b521ede
Shuriken (ACER) <tarabet@yandex.ru>
2015-10-28 16:02:24 +0200

    Dynamic products filters: 2 level filters are generated, filter works on 1-st level

--- js/directives/products-filters-drct.js
+++ js/directives/products-filters-drct.js
@@ -3,24 +3,33 @@
  */
 ngApp.directive('productsFilters', function ($compile, ajaxSvc) {
 
-    var url = "http://192.168.50.56:8080/ords/virtualbranch_ws/reference/open/Product/1";
+    //var url1 = "http://192.168.50.56:8080/ords/virtualbranch_ws/reference/open/Product/1";
+    var url2 = "http://192.168.50.56:8080/ords/virtualbranch_ws/reference/open/ProductStructure/1";
     var template, products;
 
 
     function link(scope, element, attrs) {
 
-        // Choose all sub-products and return array with them
-        function genSubArray(text){
-            var arr = [];
-            var prodObj = scope.products;
-            for (var i = 0; i < prodObj.length; i += 1) {
-                if(prodObj[i].prod_type_name === text) {
-                    prodObj[i].prod_sub_type.forEach(function(elem) {
-                        arr.push(elem);
-                    });
+        function genTopArray(productsArr) {
+            var topArr = [];
+            for (var i = 0; i < productsArr.items.length; i++) {
+                if (topArr.indexOf(productsArr.items[i].prod_type_name) === -1) {
+                    topArr.push(productsArr.items[i].prod_type_name);
                 }
             }
-            return arr;
+            return topArr;
+        }
+
+        // Choose all sub-products and return array with them
+        function genSubArray(text){
+            var subArr = [];
+            var prodObj = scope.products.items;
+            for (var i = 0; i < prodObj.length; i += 1) {
+                if(prodObj[i].prod_type_name === text) {
+                    subArr.push(prodObj[i].prod_subtype_name);
+                }
+            }
+            return subArr;
         }
 
         // Gets array with sub-products, generates links and appends them to sub-filter container
@@ -33,15 +42,16 @@ ngApp.directive('productsFilters', function ($compile, ajaxSvc) {
             }
             var elem;
             for(var i = 0; i < subArr.length; i++) {
-                elem = $('<a class="sub-item"></a>').text(subArr[i].prod_subtype_name);
+                elem = $('<a class="sub-item"></a>').text(subArr[i]);
                 parentCont.append(elem);
             }
         }
 
-        ajaxSvc.getData(url)
+        ajaxSvc.getData(url2)
 
             .then(function (response) {
-                scope.products = response.data.items;
+                scope.products = response.data;
+                scope.topArr = genTopArray(scope.products); //1-st level products categories
                 console.log('products:', scope.products);
             },
             function (response) {
@@ -60,6 +70,8 @@ ngApp.directive('productsFilters', function ($compile, ajaxSvc) {
                         $(elements).removeClass('act');
                         $(this).addClass('act');
                         scope.topChoice = $(this).text();
+                        console.log('topChoice:', scope.topChoice);
+                        scope.$apply();
                         genSubMenu(scope.topChoice); //Generate sub-menu and append it in sub-menu container
                     })
 
